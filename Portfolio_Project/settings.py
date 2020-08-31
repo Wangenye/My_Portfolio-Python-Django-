4431bbbb224623a60bf3e442e8eb926793d82be1
root <root@localhost.localdomain>
2020-08-31 22:39:50 +0300

    New style

--- Portfolio_Project/settings.py
+++ Portfolio_Project/settings.py
@@ -128,7 +128,8 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 
