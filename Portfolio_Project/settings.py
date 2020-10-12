e1a9581773fc519952eb19883a5364766719d41b
root <root@localhost.localdomain>
2020-10-12 21:54:07 +0300

    Web Optimization

--- Portfolio_Project/settings.py
+++ Portfolio_Project/settings.py
@@ -128,7 +128,7 @@ STATICFILES_DIRS = [
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 
